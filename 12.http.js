d43a041c9edd0b9ed7c3cf4b5571a7611c9d6af8
Andi Faizal <andifaizal647@gmail.com>
2022-05-28 20:55:34 +0700

    add http

--- 12.http.js
+++ 12.http.js
@@ -1,55 +1,35 @@
 const http = require('node:http');
-const net = require('node:net');
-const { URL } = require('node:url');
-
-// Create an HTTP tunneling proxy
-const proxy = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('okay');
+// const net = require('node:net');
+// const { URL } = require('node:url');
+const server = http.createServer((req, res) => {
+  //   if (req.url === '/') {
+  //     res.end('Welcome to our home page')
+  //   }
+  //   if (req.url === '/about') {
+  //     res.end('Here is our short history')
+  //   }
+  //   res.end(`
+  //   <h1>Oops!</h1>
+  // <p>We can't seem to find the page you are looking for</p>
+  // <a href="/">back home</a>
+  //   `)
+  // ###################################
+  // ###################################
+  //
+  //  IF YOU GET ERRORS WHILE USING ABOVE SETUP,
+  // SWITCH TO IF, ELSE IF, ELSE (BELOW)
+  // WE COVER THE CAUSE, LATER IN EXPRESS TUTORIAL
+  if (req.url === '/') {
+    res.end('Welcome to our home page');
+  } else if (req.url === '/about') {
+    res.end('Here is our short history');
+  } else {
+    res.end(`
+      <h1>Oops!</h1>
+      <p>We can't seem to find the page you are looking for</p>
+      <a href="/">back home</a>
+      `);
+  }
 });
-proxy.on('connect', (req, clientSocket, head) => {
-  // Connect to an origin server
-  const { port, hostname } = new URL(`http://${req.url}`);
-  const serverSocket = net.connect(port || 80, hostname, () => {
-    clientSocket.write(
-      'HTTP/1.1 200 Connection Established\r\n' +
-        'Proxy-agent: Node.js-Proxy\r\n' +
-        '\r\n'
-    );
-    serverSocket.write(head);
-    serverSocket.pipe(clientSocket);
-    clientSocket.pipe(serverSocket);
-  });
-});
 
-// Now that proxy is running
-proxy.listen(1337, '127.0.0.1', () => {
-  // Make a request to a tunneling proxy
-  const options = {
-    port: 1337,
-    host: '127.0.0.1',
-    method: 'CONNECT',
-    path: 'www.google.com:80',
-  };
-
-  const req = http.request(options);
-  req.end();
-
-  req.on('connect', (res, socket, head) => {
-    console.log('got connected!');
-
-    // Make a request over an HTTP tunnel
-    socket.write(
-      'GET / HTTP/1.1\r\n' +
-        'Host: www.google.com:80\r\n' +
-        'Connection: close\r\n' +
-        '\r\n'
-    );
-    socket.on('data', (chunk) => {
-      console.log(chunk.toString());
-    });
-    socket.on('end', () => {
-      proxy.close();
-    });
-  });
-});
+server.listen(5000);
